4961fa08645f03dc0488cf5c92ad7b82d2a6a959
roccogiordano <rocco.giordano573@gmail.com>
2024-11-30 17:23:58 +0100

    feat: variables declaration

---
+++ java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,13 @@
+package org.lessons.java.security;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        String FirstName;
+        String LastName;
+        String FavouriteColor;
+        Byte DayOfBirth;
+        Byte MonthOfBirth;
+        Short YearOfBirth;
+        Short SumOfBirth;
+    }
+}
