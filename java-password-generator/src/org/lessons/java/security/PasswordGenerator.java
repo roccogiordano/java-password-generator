2300c53348707936b0f83e6cc53b1f024c369109
roccogiordano <rocco.giordano573@gmail.com>
2024-11-30 17:46:23 +0100

    feat: added user input

--- java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
+++ java-password-generator/src/org/lessons/java/security/PasswordGenerator.java
@@ -1,13 +1,45 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
+
+        // Variables Declaration
+
         String FirstName;
         String LastName;
         String FavouriteColor;
-        Byte DayOfBirth;
-        Byte MonthOfBirth;
-        Short YearOfBirth;
-        Short SumOfBirth;
+        Integer DayOfBirth;
+        Integer MonthOfBirth;
+        Integer YearOfBirth;
+        Integer SumOfBirth;
+
+        //
+
+
+        // User Input
+
+        Scanner Input = new Scanner(System.in);
+
+        System.out.println("Type your first name...");
+        FirstName = Input.nextLine();
+
+        System.out.println("Type your last name...");
+        LastName = Input.nextLine();
+
+        System.out.println("Type your favourite color...");
+        FavouriteColor = Input.nextLine();
+
+        System.out.println("Type your date of birth (dd/mm/yyyy)...");
+        DayOfBirth = Input.nextInt();
+        MonthOfBirth = Input.nextInt();
+        YearOfBirth = Input.nextInt();
+
+        SumOfBirth = DayOfBirth + MonthOfBirth + YearOfBirth;
+
+        Input.close();
+
+        //
     }
 }
